cdc2e4ce2a5957502080ad15f82d00bf1135aec2
MarioSergio2 <ms940054@gmail.com>
2024-08-16 20:25:46 -0300

    Criação das classes e interface de Produto, dentro dos respectivos pacotes

---
+++ src/main/java/dio/padroes/model/Produto.java
@@ -0,0 +1,22 @@
+package dio.padroes.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "produtos")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private Double preco;
+    private Integer quantidade;
+}
---
+++ src/main/java/dio/padroes/repository/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package dio.padroes.repository;
+
+import dio.padroes.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+}
